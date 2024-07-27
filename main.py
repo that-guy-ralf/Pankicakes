905566c32a24c0726c49363cd8d19d60ad8d8973
R. <jugglingcode@gmail.com>
2024-07-26 23:46:22 -0400

    Pseudocode

---
+++ main.py
@@ -0,0 +1,14 @@
+from genanki import *
+from anki import *
+
+# Project Outline
+"""
+The project Pankicakes is being made to automate flashcard making with Anki.
+The goal is to make a simple program (whether gui or cli has not been decided)
+that will be able to:
+    1. Generate a flashcard from a given input
+    2. Generate a flashcard(s) from a folder recursively and
+    3. Organize decks based on decided catagories.
+"""
+
+
